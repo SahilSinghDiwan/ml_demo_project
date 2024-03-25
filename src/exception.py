7d1d9d5a5d30e630eccf1f7f969c57f275e18072
Sahil Singh <diwan.sahilsingh@gmail.com>
2024-03-25 10:11:57 +0530

    Changes to exception Handeling: Error resolved

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     
@@ -25,5 +25,5 @@ if __name__ == '__main__':
     try:
         a=1/0
     except Exception as e:
-        logging.info("Exception Checking")
+        logging.info("Exception Checking Divide by zero error.")
         raise CustomException(e,sys)
